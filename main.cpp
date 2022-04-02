9e3d28cc1dfbd07184618a24d77d0f29a69972b5
YUSUF ŞAFAK <92788564+yusuf-safak@users.noreply.github.com>
2022-04-02 13:26:52 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace std;
+int obeb(int,int);
+int okek(int,int);
+int main()
+{
+    cout<<obeb(40,30)<<endl<<okek(40,30);
+    return 0;
+}
+//ebob function
+int obeb(int a,int b){
+    int Obeb = 0;
+    if(a>b)
+    {
+        for(int  i = 1; i <= b; i++)
+        {
+            if(a%i == 0 && b%i == 0)
+            {
+                Obeb = i;
+            }
+        }
+    }
+    else
+    {
+        for(int  i = 1; i <= a; i++)
+        {
+            if(a%i == 0 && b%i == 0)
+            {
+                Obeb = i;
+            }
+        }
+    }
+    return Obeb;
+}
+//ekok function
+int okek(int a, int b)
+{
+    int Okek = (a*b)/obeb(a,b);
+    return Okek;
+}
